87c73aa9ca41a3cf6d3bfa53080703616e54f081
Harold Angenent <harold@trendwerk.nl>
2018-06-05 21:53:30 +0200

    :lipstick: Use gradient background

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-import { Font } from 'expo'
+import { Font, LinearGradient } from 'expo'
 
 export default class App extends React.Component {
   state = {
@@ -17,16 +17,17 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <View style={{
-        backgroundColor: '#fff',
+      <LinearGradient colors={['#ff3c64', '#fa6e55']} start={[0.0, 0.0]} end={[0.75, 0.75]} style={{
+        flex: 1,
         padding: 30,
         paddingTop: 80,
       }}>
         <Text style={{
+          color: 'white',
           fontFamily: this.state.fontLoaded ? 'work-sans-extra-bold' : null,
           fontSize: 56,
         }}>Hallo!</Text>
-      </View>
+      </LinearGradient>
     )
   }
 }
