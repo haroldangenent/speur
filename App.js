5960cc88cd7d7b67ddc85f328916a3728b8451ed
Harold Angenent <harold@trendwerk.nl>
2018-06-05 21:27:08 +0200

    :construction: Load work sans and say Hello

--- App.js
+++ App.js
@@ -1,23 +1,32 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { Font } from 'expo'
 
 export default class App extends React.Component {
+  state = {
+    fontLoaded: false,
+  }
+
+  async componentDidMount() {
+    await Font.loadAsync({
+      'work-sans-extra-bold': require('./fonts/WorkSans-ExtraBold.ttf'),
+    })
+
+    this.setState({ fontLoaded: true })
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View style={{
+        backgroundColor: '#fff',
+        padding: 30,
+        paddingTop: 80,
+      }}>
+        <Text style={{
+          fontFamily: this.state.fontLoaded ? 'work-sans-extra-bold' : null,
+          fontSize: 56,
+        }}>Hallo!</Text>
       </View>
-    );
+    )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
