675ef7f878d8f0534f72335048352ae89c2cfc76
Harold Angenent <harold@trendwerk.nl>
2018-06-06 13:44:12 +0200

    :construction: Add second page to swipeable view

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { StyleSheet, Text, View, Image, StatusBar } from 'react-native'
 import { Font, LinearGradient } from 'expo'
+import SwipeableViews from 'react-swipeable-views-native'
 
 export default class App extends React.Component {
   state = {
@@ -19,57 +20,73 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <LinearGradient colors={['#ff3c64', '#fa6e55']} start={[0.0, 0.0]} end={[0.75, 0.75]} style={{
-        flex: 1,
-        justifyContent: 'space-between',
-        padding: 30,
-        paddingBottom: 60,
-        paddingTop: 80,
-      }}>
+      <LinearGradient colors={['#ff3c64', '#fa6e55']} start={[0.0, 0.0]} end={[0.75, 0.75]} style={{ flex: 1 }}>
         <StatusBar hidden={true} />
 
-        <View>
-          <Text style={{
-            color: 'white',
-            fontFamily: this.state.fontsLoaded ? 'work-sans-extra-bold' : null,
-            fontSize: 56,
-            marginBottom: 25,
-          }}>Hallo!</Text>
-          <Text style={{
-            color: 'white',
-            fontFamily: this.state.fontsLoaded ? 'work-sans-extra-light' : null,
-            fontSize: 48,
-            lineHeight: 57,
-            width: 300,
-          }}>Zullen we een leuke spørtocht maken?</Text>
-        </View>
-
-        <View>
-          <Text style={{
-            color: 'white',
-            fontFamily: this.state.fontsLoaded ? 'work-sans-semi-bold' : null,
-            fontSize: 26,
-            lineHeight: 34,
-            marginBottom: 15,
-            width: 180,
-          }}>Swipe om te beginnen</Text>
+        <SwipeableViews>
           <View style={{
-            alignItems: 'center',
-            flexDirection: 'row',
-            justifyContent: 'flex-start',
+            flex: 1,
+            justifyContent: 'space-between',
+            padding: 30,
+            paddingBottom: 60,
+            paddingTop: 80,
           }}>
-            <Image source={require('./img/swipe-left.png')} style={{
-              marginRight: 15,
-            }} />
-            <Image source={require('./img/hand.png')} />
-          </View>
-        </View>
+            <View>
+              <Text style={{
+                color: 'white',
+                fontFamily: this.state.fontsLoaded ? 'work-sans-extra-bold' : null,
+                fontSize: 56,
+                marginBottom: 25,
+              }}>Hallo!</Text>
+              <Text style={{
+                color: 'white',
+                fontFamily: this.state.fontsLoaded ? 'work-sans-extra-light' : null,
+                fontSize: 48,
+                lineHeight: 57,
+                width: 300,
+              }}>Zullen we een leuke spørtocht maken?</Text>
+            </View>
 
-        <Image source={require('./img/speuren.png')} style={{
-          bottom: -30,
-          position: 'absolute',
-          right: -75,
-        }} />
+            <View>
+              <Text style={{
+                color: 'white',
+                fontFamily: this.state.fontsLoaded ? 'work-sans-semi-bold' : null,
+                fontSize: 26,
+                lineHeight: 34,
+                marginBottom: 15,
+                width: 180,
+              }}>Swipe om te beginnen</Text>
+              <View style={{
+                alignItems: 'center',
+                flexDirection: 'row',
+                justifyContent: 'flex-start',
+              }}>
+                <Image source={require('./img/swipe-left.png')} style={{
+                  marginRight: 15,
+                }} />
+                <Image source={require('./img/hand.png')} />
+              </View>
+            </View>
+
+            <Image source={require('./img/speuren.png')} style={{
+              bottom: -30,
+              position: 'absolute',
+              right: -75,
+            }} />
+          </View>
+          <View style={{
+            padding: 30,
+            paddingBottom: 60,
+            paddingTop: 80,
+          }}>
+            <Text style={{
+              color: 'white',
+              fontFamily: this.state.fontsLoaded ? 'work-sans-extra-bold' : null,
+              fontSize: 56,
+              marginBottom: 25,
+            }}>Wat is je startpunt?</Text>
+          </View>
+        </SwipeableViews>
       </LinearGradient>
     )
   }
