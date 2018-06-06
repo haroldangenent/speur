e466878841368e37dcd4d307e61dea93a0f15798
Harold Angenent <harold@trendwerk.nl>
2018-06-06 17:16:28 +0200

    :sparkles: Allow overwriting of container styles

--- components/Container.js
+++ components/Container.js
@@ -2,9 +2,9 @@ import React from 'react'
 import { View } from 'react-native'
 
 export default ({ children, style }) => (
-  <View style={[style, {
+  <View style={[{
     padding: 30,
     paddingBottom: 60,
     paddingTop: 80,
-  }]}>{children}</View>
+  }, style]}>{children}</View>
 )
