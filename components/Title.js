a453b27b0065db8a7bf54fdbc66affc361e6524b
Harold Angenent <harold@trendwerk.nl>
2018-06-06 17:16:46 +0200

    :sparkles: Include title style overwrites

--- components/Title.js
+++ components/Title.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Text } from 'react-native'
 
-export default ({ children }) => (
-  <Text style={{
+export default ({ children, style }) => (
+  <Text style={[{
     color: 'white',
     fontFamily: 'work-sans-extra-bold',
     fontSize: 56,
     marginBottom: 25,
-  }}>{children}</Text>
+  }, style]}>{children}</Text>
 )
