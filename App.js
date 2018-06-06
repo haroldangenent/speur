b4fe13f6eb47b7e0665f14e3343efa1f984bbadc
Harold Angenent <harold@trendwerk.nl>
2018-06-06 09:25:11 +0200

    :lipstick: Hide status bar

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, Text, View, Image } from 'react-native'
+import { StyleSheet, Text, View, Image, StatusBar } from 'react-native'
 import { Font, LinearGradient } from 'expo'
 
 export default class App extends React.Component {
@@ -26,6 +26,7 @@ export default class App extends React.Component {
         paddingBottom: 60,
         paddingTop: 80,
       }}>
+        <StatusBar hidden={true} />
         <View>
           <Text style={{
             color: 'white',
