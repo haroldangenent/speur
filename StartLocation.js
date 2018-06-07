88674dca9bb5d05bd784fa8f16eaaa584406a8b3
Harold Angenent <harold@trendwerk.nl>
2018-06-07 17:10:09 +0200

    :hammer: Use composition over inheritance

--- StartLocation.js
+++ StartLocation.js
@@ -1,4 +1,19 @@
+import React from 'react'
 import PickLocation from './PickLocation'
 
-export default class StartLocation extends PickLocation {
+export default class StartLocation extends React.Component {
+  transitionIn() {
+    this.picker.transitionIn()
+  }
+
+  transitionOut() {
+    this.picker.transitionIn()
+  }
+
+  render() {
+    return <PickLocation
+      onDisableSwipe={this.props.onDisableSwipe}
+      onEnableSwipe={this.props.onEnableSwipe}
+      ref={picker => this.picker = picker} />
+  }
 }
