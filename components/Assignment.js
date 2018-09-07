2205b8305d590fbe944192f8fa83532130b4baca
Harold Angenent <harold@trendwerk.nl>
2018-09-07 16:45:08 +0200

    :construction: Add arrow to tooltip

--- components/Assignment.js
+++ components/Assignment.js
@@ -10,15 +10,29 @@ export default class Assignment extends React.Component {
         backgroundColor: 'white',
         borderRadius: 3,
         bottom: '100%',
-        marginBottom: 10,
+        marginBottom: 20,
         left: -(width / 2) + 55,
         padding: 20,
-        position: 'absolute',
         shadowColor: 'black',
         shadowRadius: 10,
         shadowOpacity: 0.1,
         width: width - 60,
-      }}><Text>Nieuwe opdracht</Text></View>
+      }}>
+        <Text>Nieuwe opdracht</Text>
+        <View style={{
+          bottom: 0,
+          borderStyle: 'solid',
+          borderLeftWidth: 10,
+          borderRightWidth: 10,
+          borderTopWidth: 12,
+          borderLeftColor: 'transparent',
+          borderRightColor: 'transparent',
+          borderTopColor: 'white',
+          left: (width / 2) - 40,
+          position: 'absolute',
+          transform: [{ translateY: 12 }],
+        }} />
+      </View>
     )
   }
 }
